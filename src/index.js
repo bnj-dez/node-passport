9af5b0ded1cf2dc504c47eab2bc15aa3168d26dc
brice <prodakil@gmail.com>
2023-06-22 10:21:01 +0200

    connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,42 +1,44 @@
 import express from "express";
-import { authRouter } from "../routes/auth.js" 
-import logger from 'morgan';
-import session from 'express-session';
-import SQLiteStoreModule from 'connect-sqlite3';
-import path from 'node:path'
+import { authRouter } from "../routes/auth.js";
+import logger from "morgan";
+import session from "express-session";
+import SQLiteStoreModule from "connect-sqlite3";
+import path from "node:path";
 import { fileURLToPath } from "node:url";
-import ejs from "ejs"
-import passport from 'passport';
-import cors from 'cors';
+import ejs from "ejs";
+import passport from "passport";
+import cors from "cors";
+import("../db.js");
 
-const SQLiteStore = SQLiteStoreModule(session);
+// const SQLiteStore = SQLiteStoreModule(session);
 const app = express();
 const port = 3000;
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
-  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
-}));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new SQLiteStore({ db: "sessions.db", dir: "./var/db" }),
+  })
+);
 
-app.use(passport.authenticate('session'));
+app.use(passport.authenticate("session"));
 
-
-app.use(cors({origin: 'http://localhost:3001'}))
-app.use(express.urlencoded({extended:true}))
+app.use(cors({ origin: "http://localhost:3001" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/test', (req, res) => {
-  res.send('Ok');
+app.get("/test", (req, res) => {
+  res.send("Ok");
 });
 
-app.use('/', authRouter);
+app.use("/", authRouter);
 
 app.listen(port, () => {
   console.log("Serveur en écoute sur le port " + port);
