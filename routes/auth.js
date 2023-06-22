172f5e9a16a53a1403e8fa532a003e01bac09642
brice <prodakil@gmail.com>
2023-06-22 10:36:06 +0200

    ✨ connect to db

--- routes/auth.js
+++ routes/auth.js
@@ -1,97 +1,97 @@
-import express from "express";
-import passport from 'passport';
-import FacebookStrategy from 'passport-facebook';
-import LocalStrategy from 'passport-local';
-import crypto from 'node:crypto';
-import { db } from '../db.js';
-import dotenv from "dotenv";
+// import express from "express";
+// import passport from 'passport';
+// import FacebookStrategy from 'passport-facebook';
+// import LocalStrategy from 'passport-local';
+// import crypto from 'node:crypto';
+// import { db } from '../db.js';
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-passport.use(new LocalStrategy(function verify(username, password, cb) {
-    db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
-      if (err) { return cb(err); }
-      if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+// passport.use(new LocalStrategy(function verify(username, password, cb) {
+//     db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
+//       if (err) { return cb(err); }
+//       if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
   
-      crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-        if (err) { return cb(err); }
-        if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
-          return cb(null, false, { message: 'Incorrect username or password.' });
-        }
-        return cb(null, row);
-      });
-    });
-  }));
+//       crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+//         if (err) { return cb(err); }
+//         if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+//           return cb(null, false, { message: 'Incorrect username or password.' });
+//         }
+//         return cb(null, row);
+//       });
+//     });
+//   }));
 
 
 
-passport.use(new FacebookStrategy({
-    clientID: process.env['FACEBOOK_CLIENT_ID'],
-    clientSecret: process.env['FACEBOOK_CLIENT_SECRET'],
-    callbackURL: '/oauth2/redirect/facebook',
-    state: true
-}, function verify(accessToken, refreshToken, profile, cb) {
-    db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-    'https://www.facebook.com',
-    profile.id
-    ], function(err, row) {
-        if (err) { return cb(err); }
+// passport.use(new FacebookStrategy({
+//     clientID: process.env['FACEBOOK_CLIENT_ID'],
+//     clientSecret: process.env['FACEBOOK_CLIENT_SECRET'],
+//     callbackURL: '/oauth2/redirect/facebook',
+//     state: true
+// }, function verify(accessToken, refreshToken, profile, cb) {
+//     db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+//     'https://www.facebook.com',
+//     profile.id
+//     ], function(err, row) {
+//         if (err) { return cb(err); }
 
-        if (!row) {
-            db.run('INSERT INTO users (name) VALUES (?)', [
-            profile.displayName
-            ], function(err) {
-            if (err) { return cb(err); }
+//         if (!row) {
+//             db.run('INSERT INTO users (name) VALUES (?)', [
+//             profile.displayName
+//             ], function(err) {
+//             if (err) { return cb(err); }
 
-            var id = this.lastID;
-            db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
-                id,
-                'https://www.facebook.com',
-                profile.id
-            ], function(err) {
-                if (err) { return cb(err); }
-                var user = {
-                id: id,
-                name: profile.displayName
-                };
-                return cb(null, user);
-            });
-        });
-    } else {
-        db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
-        if (err) { return cb(err); }
-        if (!row) { return cb(null, false); }
-        return cb(null, row);
-        });
-    }
-    });
-}));
+//             var id = this.lastID;
+//             db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+//                 id,
+//                 'https://www.facebook.com',
+//                 profile.id
+//             ], function(err) {
+//                 if (err) { return cb(err); }
+//                 var user = {
+//                 id: id,
+//                 name: profile.displayName
+//                 };
+//                 return cb(null, user);
+//             });
+//         });
+//     } else {
+//         db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
+//         if (err) { return cb(err); }
+//         if (!row) { return cb(null, false); }
+//         return cb(null, row);
+//         });
+//     }
+//     });
+// }));
 
-passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-    cb(null, { id: user.id, username: user.username, name: user.name });
-});
-});
+// passport.serializeUser(function(user, cb) {
+//     process.nextTick(function() {
+//     cb(null, { id: user.id, username: user.username, name: user.name });
+// });
+// });
 
-passport.deserializeUser(function(user, cb) {
-    process.nextTick(function() {
-        return cb(null, user);
-    });
-});
+// passport.deserializeUser(function(user, cb) {
+//     process.nextTick(function() {
+//         return cb(null, user);
+//     });
+// });
 
-const authRouter = express.Router();
+// const authRouter = express.Router();
 
-authRouter.get('/login/federated/facebook', passport.authenticate('facebook'));
+// authRouter.get('/login/federated/facebook', passport.authenticate('facebook'));
 
-authRouter.get('/oauth2/redirect/facebook', passport.authenticate('facebook', {
-    successReturnToOrRedirect: '/test',
-    failureRedirect: '/login'
-}));
+// authRouter.get('/oauth2/redirect/facebook', passport.authenticate('facebook', {
+//     successReturnToOrRedirect: '/test',
+//     failureRedirect: '/login'
+// }));
 
-authRouter.post('/login/password', passport.authenticate('local'),(req,res)=>{
-    res.send('You are authenticated !')
-  });
+// authRouter.post('/login/password', passport.authenticate('local'),(req,res)=>{
+//     res.send('You are authenticated !')
+//   });
 
 
     
-export {authRouter};
+// export {authRouter};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import express from "express";
-import { authRouter } from "../routes/auth.js";
-import logger from "morgan";
+// import { authRouter } from "../routes/auth.js";
 import session from "express-session";
-import SQLiteStoreModule from "connect-sqlite3";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import ejs from "ejs";
 import passport from "passport";
 import cors from "cors";
+
 import("../db.js");
 
-// const SQLiteStore = SQLiteStoreModule(session);
 const app = express();
 const port = 3000;
 
@@ -22,7 +19,6 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
-    store: new SQLiteStore({ db: "sessions.db", dir: "./var/db" }),
   })
 );
 
@@ -32,13 +28,11 @@ app.use(cors({ origin: "http://localhost:3001" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.set("view engine", "ejs");
-
 app.get("/test", (req, res) => {
   res.send("Ok");
 });
 
-app.use("/", authRouter);
+// app.use("/",); //authRouter
 
 app.listen(port, () => {
   console.log("Serveur en écoute sur le port " + port);
